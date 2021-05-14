b36bd0340876c815c23656c297257b95373f3292
Simona Singh <simona.priya.s@gmail.com>
2021-05-14 13:39:46 +0100

    Just in case

--- watercolour/tools/saturation.py
+++ watercolour/tools/saturation.py
@@ -1,10 +1,9 @@
 from watercolour.tool import Tool
 from kivy.uix.slider import Slider
 from kivy.graphics.texture import Texture
-
 import cv2
-
 import numpy as np
+
 
 class Saturation(Tool):
     """The Saturation Tool allows users to increase/decrease saturation."""
@@ -15,36 +14,25 @@ class Saturation(Tool):
         super().__init__(path)
 
     def add_settings(self):
+        """Overrides base class to add widgets to self and bind to functions."""
         self.add_widget(self.saturationControl)
         self.saturationControl.bind(value=self.on_saturation)
 
     def remove_settings(self):
+        """"Overrides base class to remove widgets from self."""
         self.remove_widget(self.saturationControl)
 
     def on_saturation(self, instance, saturation):
+        """Handles saturationControl slider."""
         self.saturationValue = saturation
         self.update_photo(self.saturationValue)
 
     def update_photo(self, saturation):
-        self.img_source = cv2.imread(self.path, cv2.IMREAD_COLOR)#.astype(np.float32) / 255.0
-        #temp = self.img_source.copy()
+        """Creates copy of image in cv2.
+        Replaces pixels with white/grey/black depending on boundary.
+        Converts to texture and replaces image texture."""
+        self.img_source = cv2.imread(self.path, cv2.IMREAD_COLOR)
         hsv = cv2.cvtColor(self.img_source, cv2.COLOR_BGR2HSV)
-
-        #hsv[:, :, 2] = (1.0 + saturation / float(self.saturationControl.max)) * hsv[:, :, 2]
-        #hsv[:, :, 2][hsv[:, :, 2] > 1] = 1
-
-        #H, S, V = cv2.split(hsv)
-        #temp = cv2.merge([np.uint8(H), np.uint8(S*saturation), np.uint8(V)])
-
-        #temp = hsv*saturation
-
-        #for row in hsv:
-        #    for pixel in hsv:
-        #        #print(pixel)
-        #        hsv[row,pixel,1] = saturation * hsv[row,pixel,1]
-
-        #print(temp.shape)
-
 
         h, s, v = cv2.split(hsv)
         print(type(s))
@@ -53,12 +41,10 @@ class Saturation(Tool):
         s[s <= lim] += int(saturation)
         temp = cv2.merge([np.uint8(h), np.uint8(s), np.uint8(v)])
 
-
         x = cv2.cvtColor(temp, cv2.COLOR_HSV2BGR)
         w, h, c = x.shape
         texture = Texture.create(size=(h,w))
         texture.uvpos = (0, 1)
         texture.uvsize = (1, -1)
-        texture.blit_buffer(x.flatten(), colorfmt='bgr', bufferfmt='ubyte')  # ????
-        #w_img = Image(size=(w, h), texture=texture)
+        texture.blit_buffer(x.flatten(), colorfmt='bgr', bufferfmt='ubyte')
         self.ids.tool_image.texture = texture
--- watercolour/tools/thresholdingvalues.py
+++ watercolour/tools/thresholdingvalues.py
@@ -31,9 +31,8 @@ class ThresholdingValues(Tool):
         super().__init__(path)
 
     def add_settings(self):
-        """Overrides base class to add widgets to self."""
+        """Overrides base class to add widgets to self and bind to functions."""
         self.ids.lbl.text = 'Values'
-        # self.add_widget(self.img)
         self.add_widget(self.highlightLabel)
         self.add_widget(self.highlightControl)
         self.add_widget(self.shadowLabel)
@@ -41,20 +40,9 @@ class ThresholdingValues(Tool):
         self.add_widget(self.blurLabel)
         self.add_widget(self.blurControl)
 
-        # one label for slider value
-        #self.add_widget(Label(text='Slider Value'))
-        #self.brightnessValue = Label(text='0')
-        #self.add_widget(self.brightnessValue)
-
-        #self.label = Label()
-        #self.add_widget(self.label)
-        #Window.bind(mouse_pos=self.mouse_pos)
-
-        # On the slider object Attach a callback
-        # for the attribute named value
         self.blurControl.bind(value=self.on_blur)
         self.highlightControl.bind(value=self.on_highlight)
-        self.shadowControl.bind(value=self.on_shadow) # change to all one function
+        self.shadowControl.bind(value=self.on_shadow)
 
         self.shadowControl.value = self.settings.get("shadowValue")
         self.blurControl.value = self.settings.get("blurValue")
@@ -65,7 +53,6 @@ class ThresholdingValues(Tool):
         self.remove_widget(self.highlightControl)
         self.remove_widget(self.shadowControl)
         self.remove_widget(self.blurControl)
-        #self.remove_widget(self.brightnessValue)
         self.remove_widget(self.blurLabel)
         self.remove_widget(self.highlightLabel)
         self.remove_widget(self.shadowLabel)
@@ -83,19 +70,14 @@ class ThresholdingValues(Tool):
         temp[np.where((temp <= [shadow]))] = [0]  # change pixels below shadow boundary to black
         temp[np.where((temp < highlight) & (temp > shadow))] = [100]  # change remaining pixels to grey
 
-        #print(temp.shape)
         w, h = temp.shape
         texture = Texture.create(size=(h,w))
         self.flip(texture)
         texture.blit_buffer(temp.flatten(), colorfmt='luminance', bufferfmt='ubyte')  # ????
-        #w_img = Image(size=(w, h), texture=texture)
         self.ids.tool_image.texture = texture
 
-    # i.e when pressed increase the value
     def on_blur(self, instance, blur):
         """Handles blurControl slider."""
-        #print("blurb")
-        #self.brightnessValue.text = "% d" % blur
         self.blurValue = blur
         self.update_photo(self.blurValue, self.highlightValue, self.shadowValue)
 
@@ -108,10 +90,6 @@ class ThresholdingValues(Tool):
         """Handles shadowControl slider."""
         self.shadowValue = shadow
         self.update_photo(self.blurValue, self.highlightValue, self.shadowValue)
-    # The app class
-
-    def mouse_pos(self, window, pos):
-        self.label.text = "Mouse coords = " + str(pos)
 
     def get_settings(self):
         """Updates settings array and returns it for FileBrowser to save session."""
